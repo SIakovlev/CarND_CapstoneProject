29aef76c5dbab090f6b05a7caffaef67106efb15
Charlie Wartnaby <charlie@wartnaby.org>
2018-07-18 04:53:01 -0700

    Turning off image capture by default for rest of team to use

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -30,7 +30,7 @@ class TLDetector(object):
         # Some variables for development use
         # Could make these ROS params to avoid editing code for different experiments
         self.stub_return_ground_truth = True   # If set, cheat by just returning known state
-        self.grab_training_images = True       # If set, saving image files for classifier training
+        self.grab_training_images = False      # If set, saving image files for classifier training
         self.using_real_images = False         # True for ROS bag of real images, false for simulator
         self.training_image_idx = 0            # For training image unique filenames
         self.sim_image_grab_max_range = 80     # Only grab image when close to traffic light
