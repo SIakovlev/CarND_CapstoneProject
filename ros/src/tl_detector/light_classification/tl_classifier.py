a209a5a0390f2353c5fdabb6c98388a66c449319
Charlie Wartnaby <charlie@wartnaby.org>
2018-07-29 18:13:27 +0100

    Put in switch so that we can still demonstrate VGG classifier, though with F-R-CNN enabled by default

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -6,13 +6,27 @@ import time, sys
 from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import numpy as np
+import scipy
+
+# Only required for VGG classifier:
+sys.path.insert(0,"../../../training")
+import cnn_classifier_model
+
 
 DETECTION_THRESHOLD = 0.5
 
 class TLClassifier(object):
     def __init__(self):
 
-        self.is_site = True # if set then classifier for real images will be used
+        # We tried a couple of classifiers during development. The best one
+        # is Faster R-CNN, which should normally be used.
+        # We also tried a full-frame classifier based on VGG, which worked OK in
+        # the simulator, but not for real images. It's kept here as an option just
+        # so we can still demonstrate it.
+        # So choose "FRCNN" or "VGG" here. (String in case we have other alternatives later.)
+        self.classifier = "FRCNN"
+
+        self.is_site = False # if set then classifier for real images will be used
 
         # specify path to /models directory with respect to the absolute path of tl_classifier.py
         MODEL_DIR_NAME=os.path.join(os.path.dirname(__file__), 'models')
@@ -48,34 +62,44 @@ class TLClassifier(object):
                             (percent, progress_size / (1024 * 1024), speed, duration))
             sys.stdout.flush()
 
-        if os.path.isfile(fullfilename):
-            print("Model file is downloaded. Full path: {} \n".format(fullfilename))
-            print("Proceeding with graph initialisation...\n")
+        if self.classifier == "FRCNN":
+		if os.path.isfile(fullfilename):
+		    print("Model file is downloaded. Full path: {} \n".format(fullfilename))
+		    print("Proceeding with graph initialisation...\n")
+		else:
+		    print("Model file is missing, start downloading...\n")
+		    urllib.request.urlretrieve(DOWNLOAD_URL, fullfilename, reporthook)
+		    print()
+		    print("New directory was created: {}".format(MODEL_DIR_NAME))
+		    print("Model file is downloaded. Full path: {} \n".format(fullfilename))
+		    print("Proceeding with classifier initialisation...\n")
+
+		# Import tensorflow graph
+		self.detection_graph = tf.Graph()
+		with self.detection_graph.as_default():
+		    od_graph_def = tf.GraphDef()
+		    with tf.gfile.GFile(fullfilename, 'rb') as fid:
+		        serialized_graph = fid.read()
+		        od_graph_def.ParseFromString(serialized_graph)
+		        tf.import_graph_def(od_graph_def, name='')
+		    # get all necessary tensors
+		    self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+		    self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+		    self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+		    self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+		    self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
+		self.sess = tf.Session(graph=self.detection_graph)
+
+        elif self.classifier == "VGG":
+            self.sess = tf.Session()
+            self.cnn_model = cnn_classifier_model.CnnClassifierModel(self.sess, True)
+            self.image_counter = 0
+
         else:
-            print("Model file is missing, start downloading...\n")
-            urllib.request.urlretrieve(DOWNLOAD_URL, fullfilename, reporthook)
-            print()
-            print("New directory was created: {}".format(MODEL_DIR_NAME))
-            print("Model file is downloaded. Full path: {} \n".format(fullfilename))
-            print("Proceeding with classifier initialisation...\n")
+            print("Error: unknown classifier choice %s, aborting" % self.classifier)
+            sys.exit(1)
 
-        # Import tensorflow graph
-        self.detection_graph = tf.Graph()
-        with self.detection_graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(fullfilename, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
-            # get all necessary tensors
-            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-            self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-            self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-            self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
-        self.sess = tf.Session(graph=self.detection_graph)
-
-    def get_classification(self, image):
+    def get_classification_frcnn(self, image):
         """Determines the color of the traffic light in the image
 
         Args:
@@ -114,7 +138,18 @@ class TLClassifier(object):
                 sys.stderr.write("Debug: Traffic state: OFF\n")     
                 return TrafficLight.UNKNOWN
 
-        '''
+
+    def get_classification_vgg(self, image):
+        """Determines the color of the traffic light in the image using
+           full-frame VGG classifier.
+
+        Args:
+            image (cv::Mat): image containing the traffic light
+
+        Returns:
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
         # Shrink image to size required by network first, so that any further
         # operations are computationally cheaper
         image = scipy.misc.imresize(image, self.cnn_model.image_shape)
@@ -133,6 +168,7 @@ class TLClassifier(object):
             # Convert 0..3 model output to ROS light state value
             top_score = 4 # unknown
 
+        colour_enum = ['RED', 'YELLOW', 'GREEN', 'UNKNOWN', 'UNKNOWN']
         if True: # debug only
             # Useful to see scores for each class. Also saving images has been useful
             # to identify colour coding problems and so that we can see what the image
@@ -141,10 +177,18 @@ class TLClassifier(object):
             classification = "R%.2f_Y%.2f_G%.2f_U%.2f" % tuple(softmax_scores_as_list)
             filename = "run_%d_%d_%s.jpg" % (self.image_counter, top_score, classification)
             #scipy.misc.imsave(filename, image)
-            print("Debug: image %s classed:%d" % (filename, top_score))
+            print("Debug: image %s classed:%s" % (filename, colour_enum[top_score]))
             self.image_counter += 1
             
-             
         return top_score
-        '''
-        
+
+
+    def get_classification(self, image):
+        if self.classifier == "FRCNN":
+            return self.get_classification_frcnn(image)
+        elif self.classifier == "VGG":
+            return self.get_classification_vgg(image)
+        else:
+            print("Error: unknown classifier choice %s, aborting" % self.classifier)
+            sys.exit(1)
+
